c908ff927f3199a1ba1ef8ccd5ab1d992dca1598
ThePuffin <ballutvincent@gmail.com>
2018-04-19 17:06:02 +0200

    ajout dojo10-2

---
+++ dojo10-2.js
@@ -0,0 +1,44 @@
+// Ecrire une fonction kdigits qui prend en paramètre 2 nombres: n et k tel que:
+
+// kdigits(63289, 2) => On enlève les 2 plus grands nombres, puis on cherche le nombres le plus petit parmis les nombres restants: => "2"
+
+// kdigits(1032, 1) => "1" (sans le 0)
+
+// kdigits(9573, 2) => "3" (sans le 9,7)
+
+// kdigits(48576575, 3) => "4" (sans le 8,7,6)
+
+// kdigits(12131211212, 2) => "1"
+
+// kdigits(111112, 2) => "Aucun K nombre détécté" (sans le 1,2)
+
+// kdigits(1234, 4) => "Aucun K nombre détécté" (sans le 8,7,6)
+
+// kdigits(123, 4) => Erreur (avec la classe Error): "Attention, votre nombre d'extraction est plus grand que le nombre" (sans le 8,7,6)
+
+// Bonus: Modifier la fonction afin qu'elle retourne LES K nombres plus petits:
+
+// kdigits(9573, 2) => [3,5] (sans le 9,7)
+
+function kdigits(n, k) {
+  let tabN = [...n.toString()];
+
+  tabN = tabN.map(string => parseInt(string));
+
+  const tableauSimple = tabN.filter(
+    (item, pos, self) => self.indexOf(item) === pos
+  );
+
+  if (k > tabN.length) {
+    console.log(
+      "Attention, votre nombre d'extraction est plus grand que le nombre"
+    );
+  } else if ((k = tableauSimple.length)) {
+    console.log("Aucun K nombre détecté");
+  } else {
+    console.log(Math.min(...tableauSimple));
+  }
+}
+
+kdigits(632899, 4);
+kdigits(111112, 2);
